c4bf1f6eceec516a4cbf57ac96fd1948e4b3e931
Rochiyat <rochiyat@gmail.com>
2024-11-25 19:50:11 +0700

    Add moment.js dependency and implement LinkedIn activity grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,161 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import React, { useEffect } from 'react';
+import moment from 'moment';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const Home: React.FC = () => {
+  let activity: any[] = [];
+  let grid: (Date | null)[][] = Array(7).fill(Array(52).fill(null));
+  let startDate: null | Date = null;
 
-export default function Home() {
+  function getMonthName(month: number) {
+    if (month > 11) {
+      month = month % 12;
+    }
+    console.log('month', month);
+    const monthNames = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    return monthNames[month];
+  }
+
+  function getDayName(day: number) {
+    const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return dayNames[day];
+  }
+
+  function generateDateGrid(dataDates: any[]) {
+    const startDate = moment(dataDates[0], 'DD/MM/YYYY').toDate();
+    console.log('startDate', startDate);
+    const grid = [];
+
+    for (let i = 0; i < 7; i++) {
+      const days = [];
+      const currentDate = startDate;
+      console.log('currentDate', currentDate);
+      currentDate.setDate(currentDate.getDate() + i);
+
+      for (let j = 0; j < 52; j++) {
+        console.log('currentDate', moment(currentDate).format('DD/MM/YYYY'));
+        if (dataDates.includes(moment(currentDate).format('DD/MM/YYYY'))) {
+          days.push(currentDate);
+        } else {
+          days.push(null);
+        }
+        currentDate.setDate(currentDate.getDate() + 7); // Move to next week
+      }
+      grid.push(days);
+    }
+
+    return grid;
+  }
+
+  useEffect(() => {
+    fetch(
+      'https://opensheet.elk.sh/12J0NsUJfMU0I2qmPibz7aFXrJMk9SsnH1UUWyYeSQ3E/1'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        console.log('data', data);
+        const dataDates = data.map((item: any) => {
+          console.log('item', item.Date);
+          return item.Date;
+        });
+        // console.log('dataDates', dataDates);
+
+        grid = generateDateGrid(dataDates);
+        startDate = grid[0][0];
+        activity = data;
+        console.log('activity', activity);
+        console.log('startDate', startDate);
+      });
+  }, []);
+
+  function getBackgroundColor(contributions: boolean) {
+    // Fungsi untuk menentukan warna berdasarkan jumlah kontribusi
+    // Misalnya, menggunakan skala warna hijau
+    if (contributions) return '#33cc33'; // Hijau tua
+    return '#99cc99'; // Hijau muda
+  }
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      {/* Title */}
+      <h1 className="text-xl font-bold mb-6">LinkedIn Activity Grid</h1>
+      <div className="bg-white/5 border border-white/10 p-6 rounded-lg">
+        <table className="table-auto">
+          <thead>
+            <tr className="pb-2">
+              <td></td>
+              {Array.from({ length: 12 }).map((_, idx) => (
+                <td
+                  key={idx}
+                  className="text-xs text-white h-[20px]"
+                  colSpan={idx < 8 && idx % 2 === 0 ? 5 : 4}
+                >
+                  {getMonthName((startDate ?? new Date()).getMonth() + idx)}
+                </td>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {grid.map((days, idx) => (
+              <tr key={idx}>
+                <td>
+                  {(idx === 0 || idx === 2 || idx === 4) && (
+                    <div className="pr-2 h-[10px]">
+                      <span className="text-xs text-white relative">
+                        {getDayName(idx + 1)}
+                      </span>
+                    </div>
+                  )}
+                </td>
+                {days.map((week, weekIdx) => (
+                  <td key={weekIdx}>
+                    <div
+                      className={`w-2.5 h-2.5 rounded-sm ${
+                        week !== null ? 'bg-green-500' : 'bg-slate-700'
+                      }`}
+                    ></div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* Footer */}
+      <footer className="mt-6 text-sm text-gray-400">
+        Built by{' '}
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/rochiyat-rochiyat-70b67021/"
+          className="text-blue-400 underline"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          rochiyat
         </a>
+        . Source code available on{' '}
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://github.com/rochiyat"
+          className="text-blue-400 underline"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          GitHub
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        .
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
